18e41d36e294c75104dc9cdb94ded2ea581771cb
Albert <akaramean1@mail.ru>
2024-05-24 04:24:51 +0300

    делаем секурити

---
+++ src/main/java/com/bookstore/bookstore/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.bookstore.Repository;
+
+import com.bookstore.bookstore.Config.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+}
